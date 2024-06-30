8ffa3aa01e847bc070ff48d32930ed7b5a361a8c
vastina <719583581@qq.com>
2024-07-01 05:40:47 +0800

    basic playable

--- lightsoff/lightsoff.cpp
+++ lightsoff/lightsoff.cpp
@@ -1,12 +1,9 @@
 #include "lightsoff.hpp"
 
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QGridLayout>
-
 namespace vas {
 
-GameWindow::GameWindow( QWidget* parent ) : QWidget( parent )
+GameWindow::GameWindow( const std::vector<std::array<std::array<bool, 5>, 5>>& puzzles, QWidget* parent )
+  : QWidget( parent ), puzzles_( puzzles )
 {
   // this is by copilot
   // 创建主布局
@@ -17,95 +14,111 @@ GameWindow::GameWindow( QWidget* parent ) : QWidget( parent )
   whichPuzzle_->setAlignment( Qt::AlignCenter );
 
   // 将元素添加到水平布局中
-    headerLayout->addWidget( changePuzzle_ );
-    headerLayout->addStretch(); // 添加拉伸元素使关卡显示标签居中
-    headerLayout->addWidget( whichPuzzle_ );
-    headerLayout->addStretch(); // 添加拉伸元素使积分和菜单按钮靠右
-    headerLayout->addWidget( timeLabel_ );
-    headerLayout->addWidget( menuBtn_ );
+  headerLayout->addWidget( changePuzzle_ );
+  headerLayout->addStretch(); // 添加拉伸元素使关卡显示标签居中
+  headerLayout->addWidget( whichPuzzle_ );
+  headerLayout->addStretch(); // 添加拉伸元素使积分和菜单按钮靠右
+  headerLayout->addWidget( timeLabel_ );
+  headerLayout->addWidget( menuBtn_ );
   // 将水平布局添加到主布局中
-    mainLayout->addLayout( headerLayout );
-  
+  mainLayout->addLayout( headerLayout );
+
   // 创建5x5网格按钮布局
   QGridLayout* gridLayout = new QGridLayout();
   gridLayout->setSpacing( 5 ); // 设置格子之间的间距
   // 添加网格布局到主布局
-  InitBoard(gridLayout);
+  InitBoard( gridLayout );
   mainLayout->addLayout( gridLayout );
 
   // 设置窗口大小和标题
-    setFixedSize( 400, 400 );
-    setWindowTitle( "Lights Off" );
+  setFixedSize( 400, 400 );
+  setWindowTitle( "Lights Off" );
 }
 
-void GameWindow::InitCallback(const int x, const int y) {
-    QObject::connect(board_[x][y], &QPushButton::clicked, [this, x, y] {
-      //  
-      timeLabel_->setText(QString::number(timeLabel_->text().toInt() + 1));
-      //  
-      color_[x][y] = !color_[x][y];
-      board_[x][y]->setStyleSheet(color_[x][y] ? redstyle.data() : bluestyle.data());
-      if (x > 0) {
-        color_[x - 1][y] = !color_[x - 1][y];
-        board_[x - 1][y]->setStyleSheet(color_[x - 1][y] ? redstyle.data() : bluestyle.data());
-      }
-      if (x < 4) {
-        color_[x + 1][y] = !color_[x + 1][y];
-        board_[x + 1][y]->setStyleSheet(color_[x + 1][y] ? redstyle.data() : bluestyle.data());
-      }
-      if (y > 0) {
-        color_[x][y - 1] = !color_[x][y - 1];
-        board_[x][y - 1]->setStyleSheet(color_[x][y - 1] ? redstyle.data() : bluestyle.data());
-      }
-      if (y < 4) {
-        color_[x][y + 1] = !color_[x][y + 1];
-        board_[x][y + 1]->setStyleSheet(color_[x][y + 1] ? redstyle.data() : bluestyle.data());
-      }
-      //
-      if (Success()) {
-        Nextpuzzle();
-      }
-    });
-  }
+void GameWindow::InitCallback( const int x, const int y )
+{
+  QObject::connect( board_[x][y], &QPushButton::clicked, [this, x, y] {
+    //
+    timeLabel_->setText( QString::number( timeLabel_->text().toInt() + 1 ) );
+    //
+    color_[x][y] = !color_[x][y];
+    board_[x][y]->setStyleSheet( color_[x][y] ? redstyle.data() : bluestyle.data() );
+    if ( x > 0 ) {
+      color_[x - 1][y] = !color_[x - 1][y];
+      board_[x - 1][y]->setStyleSheet( color_[x - 1][y] ? redstyle.data() : bluestyle.data() );
+    }
+    if ( x < 4 ) {
+      color_[x + 1][y] = !color_[x + 1][y];
+      board_[x + 1][y]->setStyleSheet( color_[x + 1][y] ? redstyle.data() : bluestyle.data() );
+    }
+    if ( y > 0 ) {
+      color_[x][y - 1] = !color_[x][y - 1];
+      board_[x][y - 1]->setStyleSheet( color_[x][y - 1] ? redstyle.data() : bluestyle.data() );
+    }
+    if ( y < 4 ) {
+      color_[x][y + 1] = !color_[x][y + 1];
+      board_[x][y + 1]->setStyleSheet( color_[x][y + 1] ? redstyle.data() : bluestyle.data() );
+    }
+    //
+    if ( Success() ) {
+      Nextpuzzle();
+    }
+  } );
+}
 
-void GameWindow::InitBoard( QGridLayout* gridLayout ) {
-    for (int i = 0; i < 5; ++i) {
-      for (int j = 0; j < 5; ++j) {
-        board_[i][j] = new QPushButton(this);
-        board_[i][j]->setStyleSheet(redstyle.data());
-        board_[i][j]->setFixedSize(50, 50);
-        color_[i][j] = true;
-        InitCallback(i, j);
-        gridLayout->addWidget(board_[i][j], i, j);
-      }
+void GameWindow::InitBoard( QGridLayout* gridLayout )
+{
+  for ( int i = 0; i < 5; ++i ) {
+    for ( int j = 0; j < 5; ++j ) {
+      board_[i][j] = new QPushButton( this );
+      board_[i][j]->setFixedSize( 50, 50 );
+      color_[i][j] = puzzles_[puzzleIndex_][i][j];
+      board_[i][j]->setStyleSheet( color_[i][j] ? redstyle.data() : bluestyle.data() );
+      InitCallback( i, j );
+      gridLayout->addWidget( board_[i][j], i, j );
     }
   }
+}
 
-void GameWindow::Nextpuzzle() {
+void GameWindow::Nextpuzzle()
+{
+  if ( (++puzzleIndex_) >= puzzles_.size() ) {
+    puzzleIndex_ = 0;
+  }
+  for ( int i = 0; i < 5; ++i ) {
+    for ( int j = 0; j < 5; ++j ) {
+      color_[i][j] = puzzles_[puzzleIndex_][i][j];
+      board_[i][j]->setStyleSheet( color_[i][j] ? redstyle.data() : bluestyle.data() );
+    }
+  }
+  whichPuzzle_->setText( QString( "Puzzle " ).append(QChar('1' + puzzleIndex_)) );
+  timeLabel_->setText( "0" );
+}
+
+void GameWindow::menuBtnCallback()
+{
+  QObject::connect( menuBtn_, &QPushButton::clicked, [] {
     // do something
-  }
+  } );
+}
 
-void GameWindow::menuBtnCallback() {
-    QObject::connect(menuBtn_, &QPushButton::clicked, [] {
-      // do something
-    });
-  }
+void GameWindow::changePuzzleCallback()
+{
+  QObject::connect( changePuzzle_, &QPushButton::clicked, [] {
+    // do something
+  } );
+}
 
-void GameWindow::changePuzzleCallback() {
-    QObject::connect(changePuzzle_, &QPushButton::clicked, [] {
-      // do something
-    });
-  }
-
-bool GameWindow::Success() {
-    for (int i = 0; i < 5; ++i) {
-      for (int j = 0; j < 5; ++j) {
-        if (color_[i][j]) {
-          return false;
-        }
+bool GameWindow::Success()
+{
+  for ( int i = 0; i < 5; ++i ) {
+    for ( int j = 0; j < 5; ++j ) {
+      if ( !color_[i][j] ) {
+        return false;
       }
     }
-    return true;
   }
+  return true;
+}
 
 } // namespace vas
--- lightsoff/lightsoff.hpp
+++ lightsoff/lightsoff.hpp
@@ -4,48 +4,49 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QGridLayout>
-#include <qtmetamacros.h>
+
+#include <array>
+#include <vector>
 
 namespace vas {
 
-constexpr static std::string_view bluestyle {
-  "background-color: darkblue;"
-    "width: 50px;"
-    "height: 50px;"
-  };
-  constexpr static std::string_view redstyle {
-  "background-color: red;"
-    "width: 50px;"
-    "height: 50px;"
-  };
+constexpr static std::string_view bluestyle { "background-color: darkblue;"
+                                              "width: 50px;"
+                                              "height: 50px;" };
+constexpr static std::string_view redstyle { "background-color: red;"
+                                             "width: 50px;"
+                                             "height: 50px;" };
 
 class GameWindow : public QWidget
 {
   Q_OBJECT
 
 private:
-  QPushButton* menuBtn_ {new QPushButton( "☰", this )};
-  QPushButton* changePuzzle_ {new QPushButton( "Change Puzzle", this )};
-  QLabel* whichPuzzle_ {new QLabel( "Puzzle 1", this )};
-  QLabel* timeLabel_ {new QLabel("0", this) };  // how many times you click in one puzzle
+  QPushButton* menuBtn_ { new QPushButton( "☰", this ) };
+  QPushButton* changePuzzle_ { new QPushButton( "Change Puzzle", this ) };
+  QLabel* whichPuzzle_ { new QLabel( "Puzzle 1", this ) };
+  QLabel* timeLabel_ { new QLabel( "0", this ) }; // how many times you click in one puzzle
 private:
-  void menuBtnCallback() ;
-  void changePuzzleCallback() ;
+  void menuBtnCallback();
+  void changePuzzleCallback();
 
 private:
   QPushButton* board_[5][5] {};
   bool color_[5][5] {};
-  void InitCallback(const int, const int) ;
-  void InitBoard( QGridLayout* ) ;
+  void InitCallback( const int, const int );
+  void InitBoard( QGridLayout* );
 
 private:
-  bool Success() ;
-  void Nextpuzzle() ;
+  unsigned puzzleIndex_ { 0 };
+  const std::vector<std::array<std::array<bool, 5>, 5>>& puzzles_;
+  bool Success();
+  void Nextpuzzle();
+// I need to define a save game method
+
 public:
-  GameWindow( QWidget* = nullptr );
+  GameWindow( const std::vector<std::array<std::array<bool, 5>, 5>>&, QWidget* = nullptr );
 };
 
 }
-
 
 #endif
